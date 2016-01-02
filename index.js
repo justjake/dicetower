7ed304c98283b2d21611eaa7006228c42ee7d4f2
Jake Teton-Landis <jake.tl@airbnb.com>
2016-01-01 21:07:02 -0800

    init

---
+++ index.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+/*
+ * dice rolling using a nearley parser
+ */
+
+import nearley from 'nearley';
+import grammar from './generated-grammar';
+
+// returns a syntax tree from a string like "(1d4 + 2)^10"
+function parse(string) {
+  const parser = new nearley.Parser(grammar.ParserRules, grammar.ParserStart);
+  parser.feed(string);
+  return parser.results;
+}
+
+// takes a syntax tree and evaluate it, producing a result object like
+// {
+//   result :: Number,
+//   explanation :: MarkdownString
+// }
+function evaluate(tree) {
+  return tree
+}
+
+main() {
+  const input = process.argv[2]
+  console.log(`input: ${input}`)
+  const tree = parse(input)
+  console.log('--- tree ---')
+  console.log(JSON.stringify(tree, null, "  "))
+  console.log('--- result ---')
+  console.log(evaluate(tree))
+}
