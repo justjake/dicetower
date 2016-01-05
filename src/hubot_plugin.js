d3c913d67c990db12bde68305d11014d949b5aea
Jake Teton-Landis <jake.tl@airbnb.com>
2016-01-04 18:42:00 -0800

    huuubotttt script... ?

--- src/hubot_plugin.js
+++ src/hubot_plugin.js
@@ -1,9 +1,9 @@
 /*
 # Description:
-#   Allows Hubot to roll dice
+#   Advanced dice rolling calculator for Huuuuuboooot
 #
 # Dependencies:
-#   None
+#   managed by NPM
 #
 # Configuration:
 #   None
@@ -11,9 +11,39 @@
 # Commands:
 #   hubot roll (die|one) - Roll one six-sided dice
 #   hubot roll dice - Roll two six-sided dice
-#   hubot roll <x>d<y> - roll x dice, each of which has y sides
-#   hubot expected <x>d<y>
+#   hubot roll [-v verbose] [-e expected]  DICE_EXPRESSION - roll a bunch of dice and sum them up. Eg `!roll (2d(3d8) + 5d[1 5 7 9]) / 12`
+#   hubot expected DICE_EXPRESSION - get the expected value of a dice expression
 #
 # Author:
 #   jake teton-landis <just.1.jake@gmail.com>
 */
+
+import { report, contains } from './'
+
+const dont_parse = ['die', 'one', 'dice'];
+
+module.exports = function DiceRollingScript(robot) {
+  robot.respond(/roll (die|one)/i, (msg) => msg.reply(report('1d6')));
+
+  robot.respond(/roll dice/i, (msg) => msg.reply(report('2d6')))
+
+  robot.respond(/adv/i, (msg) => msg.reply(report('2d20')))
+
+  robot.respond(/rng (\d+|\d*\.\d+)/, (msg) => {
+    const num = praseFloat(msg.match[1]);
+    let reply;
+    if (num <= Math.random()) {
+      msg.reply("you live by the rng");
+    } else {
+      msg.reply("you die by the rng");
+    }
+  });
+
+  robot.respond(/roll (.+)$/i, (msg) => {
+    // make sure this isn't roll die|one|dice
+    if (contains(msg.match[1], dont_parse)) return;
+    msg.reply(report(msg.match[1]));
+  });
+
+  robot.respond(/expected (.+)/i, (msg) => msg.reply(report(msg.match[1], true)))
+}
--- src/index.js
+++ src/index.js
@@ -27,33 +27,35 @@ export function evaluate(almostTree) {
   return tree;
 }
 
-function contains(needle, haystack) {
+export function contains(needle, haystack) {
   return haystack.indexOf(needle) > -1;
 }
 
 // render the annotation of a tree
-export function render(almostTree, rolls = true, expected = true) {
+export function render(almostTree, rolls = true, expected = true, expectedOnly = false) {
   const tree = unwrap(almostTree);
   //console.log('render', tree)
-  debugger;
   if (tree === null) return 'NULL';
   if (tree.src) return render(tree.src, rolls, expected);
   // special handling for dice rolls
   if (tree.rolls) {
-    let diceRender = `${sum(tree.rolls)}`
+    let diceRender = expectedOnly ?
+      `${sum(tree.expectedRolls)}` :
+      `${sum(tree.rolls)}`
     if (rolls || expected) diceRender += ' (';
     if (rolls) diceRender += `rolled ${tree.request}: ${tree.rolls.join('+')}`;
     if (rolls && expected) diceRender += ', ';
-    if (expected) diceRender += `expected ${sum(tree.expectedRolls)}: ${tree.rolls.length}*${tree.expectedRolls[0]}`;
+    if (expected && !expectedOnly) diceRender += `expected ${sum(tree.expectedRolls)}: `
+    if (expected) diceRender += `${tree.rolls.length}*${tree.expectedRolls[0]}`;
     if (rolls || expected) diceRender += ')';
     return diceRender;
   }
   if (Array.isArray(tree)) {
     // handle paren arrays
     if (tree[0] === OPEN_PAREN && tree[tree.length - 1] === CLOSE_PAREN) {
-      return '(' + render(tree.slice(1, -1), rolls, expected) + ')';
+      return '(' + render(tree.slice(1, -1), rolls, expected, expectedOnly) + ')';
     }
-    return tree.map(x => render(x, rolls, expected)).join(' ');
+    return tree.map(x => render(x, rolls, expected, expectedOnly)).join(' ');
   }
 
   return tree;
@@ -73,10 +75,15 @@ export function run(input) {
   const tree = evaluate(parse(input));
   console.log('--- tree ---')
   console.log(JSON.stringify(tree, null, "  ")) // unwrap(unwrap(tree).src), null, "  "))
-  console.log('--- result ---')
-  console.log(`${tree.value} (expected ${tree.expected})`);
-  console.log('--- annotation ---')
-  console.log(render(tree, true, false));
+  console.log('--- report ---')
+  console.log(report(input));
+}
+
+export function report(input, useExpectedValue = false) {
+  const tree = evaluate(parse(input));
+  const asRolled = useExpectedValue ? render(tree, false, true, true) : render(tree, true, false, false);
+  const seperator = asRolled.length < 30 ? ' ' : '\n';
+  return `*${useExpectedValue ? tree.expected : tree.value}*${seperator}(${asRolled})`;
 }
 
 function main() {
